b8a2e52f4b30051000cc0d3efa1448f58009351d
bijujo <biju.joseph@gmail.com>
2019-04-07 18:12:22 -0500

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,9 +2,9 @@ variable "public_key" {
   default = "/home/ec2-user/terraform/keys/testkey.pub"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
 
-variable "aws_region" {
-	default = "us-east-1"
-	}
+# variable "aws_region" {
+# 	default = "us-east-1"
+# 	}
